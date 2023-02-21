1b4a5d0462a559b6e9997daa0258cb2cce20d60c
Benjamin Thomsen <info@benjaminthomsen.de>
2023-02-21 14:39:12 +0100

    gitkraken

---
+++ BE-websocket/server.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const express = require('express')
+const http = require("http")
+const WebSocket = require('ws')
+var app = express()
+
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server });
+
+const port = 8080
+
+wss.on('connection', function connection(ws) {
+    ws.binaryType = 'arraybuffer';
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data.toString())
+            }
+        });
+    });
+
+    ws.send('You are connected!');
+});
+
+server.listen(port, () => {
+    console.log(`Websocket server started on port ` + port);
+});
